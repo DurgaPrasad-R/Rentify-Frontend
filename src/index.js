6c595f8b7ae3a6f0facdff5cdae1bef04f823b1c
guna shekar <21pa1a0540@vishnu.edu.in>
2024-06-19 14:43:01 +0530

    Added a Chat Page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Home from "./pages/Home";
+import ChatApp from "./pages/ChatApp";
+import PropertyCard from "./components/PropertyCard";
 import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import Temp from "./pages/Temp";
 
@@ -13,6 +15,13 @@ const router = createBrowserRouter([
     children: [{ index: true, element: <Home /> },{
       path: "temp",
       element: <Temp />,
+    },{
+      path: "property",
+      element: <PropertyCard />,
+    },
+    {
+      path: "chat",
+      element: <ChatApp/>
     }],
   },
 ]);
