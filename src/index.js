d09d9e8c33cb9563f7248751a95ade949eac9507
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-06-19 16:39:00 +0530

    Changes in index file

--- src/index.js
+++ src/index.js
@@ -5,24 +5,28 @@ import App from "./App";
 import Home from "./pages/Home";
 import ChatApp from "./pages/ChatApp";
 import PropertyCard from "./components/PropertyCard";
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Temp from "./pages/Temp";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ index: true, element: <Home /> },{
-      path: "temp",
-      element: <Temp />,
-    },{
-      path: "property",
-      element: <PropertyCard />,
-    },
-    {
-      path: "chat",
-      element: <ChatApp/>
-    }],
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "temp",
+        element: <Temp />,
+      },
+      {
+        path: "property",
+        element: <PropertyCard />,
+      },
+      {
+        path: "chat",
+        element: <ChatApp />,
+      },
+    ],
   },
 ]);
 
